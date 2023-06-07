acf8d27644f699f11ea2496cfeaaf5684d9469b4
Florian Bachinger <flo.bachinger@gmail.com>
2023-06-07 12:15:54 +0200

    report on runtime, replace plot labels

--- experiments/7-analyze_results.py
+++ experiments/7-analyze_results.py
@@ -7,13 +7,18 @@ import pandas as pd
 import numpy as np
 
 summary_best = pd.read_csv('./experiments/repeated_best_config_results/summary_best.csv')
-summary_best['Configuration'] = ['best configuration'] * len(summary_best)
+summary_best['Configuration'] = ['best parameters'] * len(summary_best)
+print(f"average runtime [best configuration] {np.mean(summary_best[ summary_best['Successful'] == True]['Runtime'])}" )
 
 summary_degree2= pd.read_csv('./experiments/repeated_best_config_results/summary_degree2.csv')
 summary_degree2['Configuration'] = ['degree 2'] * len(summary_degree2)
+print(f"average runtime [degree 2] {np.mean(summary_degree2[ summary_degree2['Successful'] == True]['Runtime'])}" )
+
 
 summary_degree3 = pd.read_csv('./experiments/repeated_best_config_results/summary_degree3.csv')
 summary_degree3['Configuration'] = ['degree 3'] * len(summary_degree3)
+print(f"average runtime [degree 3] {np.mean(summary_degree3[ summary_degree3['Successful'] == True]['Runtime'])}" )
+
 
 summary = pd.concat([summary_best,summary_degree2, summary_degree3])
 summary['SampleSize'] = [ filename.split('sample_size')[1].split('_')[0] for filename in summary['DataSourceFile']]
