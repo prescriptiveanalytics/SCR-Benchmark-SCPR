f66645cf150e83858cbf186f46f7a13303ce7ed3
Florian Bachinger <flo.bachinger@gmail.com>
2025-03-14 16:02:28 +0100

    fix size of heatmap to include x label, show only median in dotplot

--- results/6-dotplot.py
+++ results/6-dotplot.py
@@ -19,47 +19,47 @@ font = {'size'   : 9}
 
 mpl.rc('font', **font)
 
-results = pd.read_csv('./results/4-R2Score-and-Violations.csv')
+results_scpr = pd.read_csv('./results/4-R2Score-and-Violations.csv')
 
-print(np.unique(results['EquationName']))
-print(len(np.unique(results['EquationName'])))
+print(np.unique(results_scpr['EquationName']))
+print(len(np.unique(results_scpr['EquationName'])))
 
-results['ConstraintsCount'].fillna(1, inplace=True)
-results['ConstraintsDerivative1Count'].fillna(1, inplace=True)
-results['ConstraintsDerivative2Count'].fillna(1, inplace=True)
-results['ConstraintsViolated'].fillna(1, inplace=True)
-results['ConstraintsViolatedDerivative1Count'].fillna(1, inplace=True)
-results['ConstraintsViolatedDerivative2Count'].fillna(1, inplace=True)
-results['R2_Test'].fillna(-10, inplace=True)
+results_scpr['ConstraintsCount'].fillna(1, inplace=True)
+results_scpr['ConstraintsDerivative1Count'].fillna(1, inplace=True)
+results_scpr['ConstraintsDerivative2Count'].fillna(1, inplace=True)
+results_scpr['ConstraintsViolated'].fillna(1, inplace=True)
+results_scpr['ConstraintsViolatedDerivative1Count'].fillna(1, inplace=True)
+results_scpr['ConstraintsViolatedDerivative2Count'].fillna(1, inplace=True)
+results_scpr['R2_Test'].fillna(-10, inplace=True)
 
-results['ConstraintsAchievedScaled'] = (1-(results['ConstraintsViolated']/results['ConstraintsCount']) ) * 100
-results['ConstraintsAchievedDerivative1Scaled'] = (1-(results['ConstraintsViolatedDerivative1Count']/results['ConstraintsDerivative1Count'])) * 100
-results['ConstraintsAchievedDerivative2Scaled'] = (1-(results['ConstraintsViolatedDerivative2Count']/results['ConstraintsDerivative2Count'])) * 100
+results_scpr['ConstraintsAchievedScaled'] = (1-(results_scpr['ConstraintsViolated']/results_scpr['ConstraintsCount']) ) * 100
+results_scpr['ConstraintsAchievedDerivative1Scaled'] = (1-(results_scpr['ConstraintsViolatedDerivative1Count']/results_scpr['ConstraintsDerivative1Count'])) * 100
+results_scpr['ConstraintsAchievedDerivative2Scaled'] = (1-(results_scpr['ConstraintsViolatedDerivative2Count']/results_scpr['ConstraintsDerivative2Count'])) * 100
 
-results['ConstraintsAchievedScaled'].fillna(100, inplace=True)
-results['ConstraintsAchievedDerivative1Scaled'].fillna(100, inplace=True)
-results['ConstraintsAchievedDerivative2Scaled'].fillna(100, inplace=True)
+results_scpr['ConstraintsAchievedScaled'].fillna(0, inplace=True)
+results_scpr['ConstraintsAchievedDerivative1Scaled'].fillna(0, inplace=True)
+results_scpr['ConstraintsAchievedDerivative2Scaled'].fillna(0, inplace=True)
 
-results['SampleSize'] = [ float(filename.split('sample_size')[1].split('_')[0]) for filename in results['DataSourceFile']]
-results['NoiseLevel'] = [ float(filename.split('noise_level')[1].split('_')[0]) for filename in results['DataSourceFile']]
-results['CountHelper'] = [1] * len(results)
-results['EquationName'] = results['EquationName'].astype("category")
-results['EquationName'] = results['EquationName'].cat.set_categories(FEYNMAN_SRSD_HARD)
+results_scpr['SampleSize'] = [ float(filename.split('sample_size')[1].split('_')[0]) for filename in results_scpr['DataSourceFile']]
+results_scpr['NoiseLevel'] = [ float(filename.split('noise_level')[1].split('_')[0]) for filename in results_scpr['DataSourceFile']]
+results_scpr['CountHelper'] = [1] * len(results_scpr)
+results_scpr['EquationName'] = results_scpr['EquationName'].astype("category")
+results_scpr['EquationName'] = results_scpr['EquationName'].cat.set_categories(FEYNMAN_SRSD_HARD)
 
-results = results[results['Successful'] == True]
+results_scpr = results_scpr[results_scpr['Successful'] == True]
 
-print(results[['ConstraintsCount','ConstraintsViolated','ConstraintsAchievedScaled']])
-results = results[['EquationName','Configuration','SampleSize','NoiseLevel', 'R2_Test',"ConstraintsAchievedScaled","ConstraintsAchievedDerivative1Scaled","ConstraintsAchievedDerivative2Scaled" ]]
-# results = results.groupby(
-#     ['EquationName','Configuration','SampleSize','NoiseLevel' ]).reset_index()
-results.index = pd.RangeIndex(len(results.index))
+print(results_scpr[['ConstraintsCount','ConstraintsViolated','ConstraintsAchievedScaled']])
+results_scpr = results_scpr[['EquationName','SampleSize','NoiseLevel', 'R2_Test',"ConstraintsAchievedScaled","ConstraintsAchievedDerivative1Scaled","ConstraintsAchievedDerivative2Scaled" ]]
+results_scpr = results_scpr.groupby(
+    ['EquationName','SampleSize','NoiseLevel' ]).median().reset_index()
+results_scpr.index = pd.RangeIndex(len(results_scpr.index))
 
-results = results.sort_values("EquationName", ascending=False)
+results_scpr = results_scpr.sort_values("EquationName", ascending=False)
 
 # df = results[ results['SampleSize'] == 100 ]
 
 for sampleSize in [1000,10000]:
-  df = results[ results['SampleSize'] == sampleSize ]
+  df = results_scpr[ results_scpr['SampleSize'] == sampleSize ]
 
   df = df.sort_values(['EquationName','SampleSize','NoiseLevel'])
   g = sns.PairGrid(df,
